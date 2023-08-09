a333fba210f53f583cf8bb9b715279a4591d8d03
lephuhung <lephuhung77@gmail.com>
2023-08-09 09:29:47 +0700

    update migration

--- Database/init-db.sql
+++ Database/init-db.sql
@@ -1,19 +1,51 @@
--- CREATE TYPE
-DROP TYPE IF EXISTS genre;
-CREATE TYPE genre AS ENUM (
-    'ADVENTURE',
-    'HORROR',
-    'COMEDY',
-    'ACTION',
-    'SPORTS'
+CREATE TABLE agents(
+    id SERIAL PRIMARY KEY,
+    name text,
+    token text,
+    zalo_name text,
+    zalo_number_target text,
+    webhook_id integer,
+    created_at TIMESTAMP,
+    ended_at TIMESTAMP NULL
 );
 
--- CREATE TABLE
-DROP TABLE IF EXISTS movies;
-CREATE TABLE movies (
+CREATE TABLE webhooks(
     id SERIAL PRIMARY KEY,
-    title VARCHAR NOT NULL,
-    release_year SMALLINT,
-    genre genre,
-    price NUMERIC(4, 2)
+    url_webhook text,
+    webhook_name text,
+    created_at TIMESTAMP,
+    ended_at TIMESTAMP NULL
+);
+
+CREATE TABLE logger (
+    id SERIAL PRIMARY KEY,
+    IP text NULL,
+    user_agents TEXT NULL,
+    device TEXT NULL,
+    IP_Info text NULL,
+    filename text NULL,
+    token text NULL,
+    timestamp TIMESTAMP NULL,
+    created_at TIMESTAMP
+);
+
+CREATE TABLE logger_error(
+    id SERIAL PRIMARY KEY,
+    IP text,
+    user_agents TEXT NULL,
+    device TEXT,
+    IP_Info text NULL,
+    filename text NULL,
+    token text NULL,
+    timestamp TIMESTAMP NULL,
+    created_at TIMESTAMP
+);
+
+CREATE TABLE Users(
+    id SERIAL PRIMARY KEY,
+    username text,
+    password text,
+    is_active boolean,
+    created_at TIMESTAMP,
+    updated_at TIMESTAMP
 );
